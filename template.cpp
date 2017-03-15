42e5c9a0f6529a6d52b9ae62bed60787411783b2
chakku <chakkurai0724@outlook.jp>
2017-03-15 21:23:15 +0900

    comment out first input because it's not good for debug

--- template.cpp
+++ template.cpp
@@ -7,13 +7,14 @@ using namespace std;
 #define LINF LLONG_MAX/3
 #define MP make_pair
 #define PB push_back
+#define pb push_back
 #define EB emplace_back
 #define ALL(v) (v).begin(),(v).end()
 #define all(v) ALL(v)
+#define sz(x) (int)(x).size()
 #define debug(x) cerr<<#x<<":"<<x<<endl
 #define debug2(x,y) cerr<<#x<<","<<#y":"<<x<<","<<y<<endl
-#define CININIT cin.tie(0),ios::sync_with_stdio(false)
-struct fin{ fin(){ cin.tie(0); ios::sync_with_stdio(false); } } fin_;
+//struct fin{ fin(){ cin.tie(0); ios::sync_with_stdio(false); } } fin_;
 struct Double{ double d; explicit Double(double x) : d(x){} };
 ostream& operator<<(ostream& os,const Double x){ os << fixed << setprecision(20) << x.d; return os; }
 template<typename T> ostream& operator<<(ostream& os,const vector<T>& vec){ os << "["; for(const auto& v : vec){ os << v << ","; } os << "]"; return os; }
